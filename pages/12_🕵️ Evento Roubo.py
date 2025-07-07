eff20791808453c58e249b377ad3f7eea7a52966
Bruno Tinti <tinti400@gmail.com>
2025-07-06 21:57:05 -0300

    🚀 Commit universal: atualização geral do projeto

--- pages/12_🕵️ Evento Roubo.py
+++ pages/12_🕵️ Evento Roubo.py
@@ -290,7 +290,6 @@ if evento.get("finalizado"):
 
     if isinstance(resumo, list) and resumo:
         df = pd.DataFrame(resumo)
-       st.dataframe(df, width=1000, height=500)
-
+        st.dataframe(df, width=1000, height=500)
     else:
         st.info("Nenhuma movimentação registrada.")
